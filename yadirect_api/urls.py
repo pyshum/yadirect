842019c41a08dbfcbd3dd11a0a14fe40bdcc12f9
pyshum <shumob@yandex.ru>
2019-11-28 15:38:41 +0300

    static files

--- yadirect_api/urls.py
+++ yadirect_api/urls.py
@@ -33,10 +33,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Статика работает при выключенном DEBUG
 if settings.DEBUG is False:
-    urlpatterns += [path('media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT, }),
+    urlpatterns += [#path('media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT, }),
                     path('static/(?P<path>.*)', serve, {'document_root': settings.STATIC_ROOT}), ]
