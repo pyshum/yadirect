993a8324eafe7481534e8fd147d7b4448aaa4141
pyshum <shumob@yandex.ru>
2019-12-02 19:07:42 +0300

    data representation

--- yadirect_api/serializers.py
+++ yadirect_api/serializers.py
@@ -5,6 +5,15 @@ from yadirect_api.models import ApiData
 
 class ApiDataSerializer(serializers.HyperlinkedModelSerializer):
 
+    def to_representation(self, obj):
+        result = {}
+        row = obj.data.get('rows')
+        header = obj.data.get('header')
+        if row and len(row) != 0:
+            result = dict(zip(header, row[0]))
+        return result
+
     class Meta:
         model = ApiData
-        fields = ('pk', 'data', 'created_at')
+        fields = '__all__'
+        # fields = ('pk', 'data', 'created_at')
